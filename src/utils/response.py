cf47ffc07a68f5af289fb543e83993bc0c02969e
Zeynep Dağtekin <118675856+zeynepvera@users.noreply.github.com>
2025-08-21 09:34:03 +0300

    Update response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
-
+from components.Package.src.models.PackageModel import ColorDominantResponse, ColorDominantOutputs, ColorDominant
+from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, OutputImage
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+    Outputs = ColorDominantOutputs(outputImage=outputImage)
+    colorDominantResponse = ColorDominantResponse(outputs=Outputs)
+    colorDominant = ColorDominant(value=colorDominantResponse)
+    executor = ConfigExecutor(value=colorDominant)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
