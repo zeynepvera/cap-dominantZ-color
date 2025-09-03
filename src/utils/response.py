f739a3eceafd2e3fb5997bce40669037b9aa42cb
Zeynep Dağtekin <118675856+zeynepvera@users.noreply.github.com>
2025-09-03 09:14:22 +0300

    Refactor response.py to use DominantColor models

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,14 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from capsules.ColorDominant.src.models.PackageModel import ColorDominantResponse, ColorDominantOutputs, ColorDominant, OutputDominantColor
-from capsules.ColorDominant.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor
+from capsules.DominantColor.src.models.PackageModel import DominantColorResponse, DominantColorOutputs, DominantColor, OutputDominantColor
+from capsules.DominantColor.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor
 
 def build_response(context):
     dominant = OutputDominantColor(value=context.dominantColor)
-    outputs = ColorDominantOutputs(dominantColor=dominant)
-    colorDominantResponse = ColorDominantResponse(outputs=outputs)
-    colorDominant = ColorDominant(value=colorDominantResponse)
-    executor = ConfigExecutor(value=colorDominant)
+    outputs = DominantColorOutputs(dominantColor=dominant)
+    dominantColorResponse = DominantColorResponse(outputs=outputs)
+    dominantColor = DominantColor(value= dominantColorResponse)
+    executor = ConfigExecutor(value= dominantColor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
